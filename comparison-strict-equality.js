c6546e1de60e352371cd1e84b8dbb3812b789fee
Phermin <jose.fermin.1986@gmail.com>
2022-07-11 23:32:35 -0300

    Comparación de valores y tipos

---
+++ comparison-strict-equality.js
@@ -0,0 +1,9 @@
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+
+function comparadorValores(a,b) { 
+    if (a===b) {
+        return true;
+    } else{
+        return false;
+    }
